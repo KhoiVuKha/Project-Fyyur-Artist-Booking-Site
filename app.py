408736e29ae72bf09d6edffd80017626180b649c
Khoi Vu <khoivukha0509@gmail.com>
2023-03-05 20:55:05 +0700

    Done connect Web App to a PostgreSQL database

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
@@ -22,6 +23,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.

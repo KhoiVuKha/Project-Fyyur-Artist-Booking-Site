413f9693b88fc0270dc9ab79a822da00b56ffa52
Khoi Vu <khoivukha0509@gmail.com>
2023-03-08 14:02:53 +0700

    Done implement function to search for Artist by name

--- app.py
+++ app.py
@@ -275,16 +275,20 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  search_term = request.form.get('search_term', '').strip()
+  results = Artist.query.filter(Artist.name.ilike('%{}%'.format(search_term))).all()
+
+  artist_list = []
+  for artist in results:
+    artist_list.append({
+      "id": artist.id,
+      "name": artist.name,
+      #TODO: "num_upcoming_shows": 
+    })
+    
+  response = {
+    "count": len(artist_list),
+    "data": artist_list
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
